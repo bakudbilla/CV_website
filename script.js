21dfff487fe33abadfab6b2eb1257983fcfffe01
Bernice <berniceawinpang@gmail.com>
2024-02-21 20:52:32 +0200

    resume website

--- script.js
+++ script.js
@@ -8,22 +8,3 @@ setProgressBarWidth('html-bar', 100);
 setProgressBarWidth('css-bar', 95);
 setProgressBarWidth('js-bar', 80);
 setProgressBarWidth('py-bar', 70);
-const myButton = document.getElementById('myButton');
-
-// Add event listener for hover
-myButton.addEventListener('mouseenter', function() {
-    this.style.backgroundColor = '#0056b3';
-});
-
-myButton.addEventListener('mouseleave', function() {
-    this.style.backgroundColor = '#007bff';
-});
-
-// Add event listener for click
-myButton.addEventListener('mousedown', function() {
-    this.style.backgroundColor = '#004080';
-});
-
-myButton.addEventListener('mouseup', function() {
-    this.style.backgroundColor = '#007bff';
-});
